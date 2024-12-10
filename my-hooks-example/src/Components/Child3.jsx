a1644f6126c9a46643bf6f64c335be3a3594c5ba
Deepanshu <paldipanshu060@gmail.com>
2024-12-10 15:26:51 +0530

    Childs

---
+++ my-hooks-example/src/Components/Child3.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Child3 = ({student}) => {
+  return (
+    <div>
+    <h1>Child3 Component</h1>
+    <h2>
+        Name={student.name}<br/>
+        Age={student.age}
+    </h2>
+</div>
+  )
+}
+
+export default Child3
--- my-hooks-example/src/Components/Contextex.jsx
+++ my-hooks-example/src/Components/Contextex.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import Child1 from './Child1'
 const Contextex = () => {
+    const student={
+        name:"XYS",
+        age:20
+    }
   return (
-    <div>Contextex</div>
+    <div>
+        <h1>Parent Component</h1>
+        <Child1 student={student}/>
+    </div>
   )
 }
+
 
 export default Contextex
