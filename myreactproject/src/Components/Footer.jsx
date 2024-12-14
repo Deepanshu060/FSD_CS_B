4e107bb2253f4e0bd6cb6549c85a324ae6a9bc2f
Deepanshu <paldipanshu060@gmail.com>
2024-12-14 18:18:04 +0530

    commited

---
+++ myreactproject/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <h3><center>Design and Developed By &copy; Deepanshu</center></h3>
+    </div>
+  )
+}
+
+export default Footer
---
+++ myreactproject/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1><center>ABESEC Ghaziabad</center></h1>
+    </div>
+  )
+}
+
+export default Header
