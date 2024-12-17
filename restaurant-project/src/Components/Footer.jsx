c27594bba934e5fd5e2f218d56ff717b4460773a
Deepanshu <paldipanshu060@gmail.com>
2024-12-17 15:37:27 +0530

    restaurant

---
+++ restaurant-project/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <h1><center>Design and Developed by Deepanshu</center></h1>
+    </div>
+  )
+}
+
+export default Footer
---
+++ restaurant-project/src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const Header = () => {
+  return (
+    <div>
+        <h1><center>ABES Restaurant, Ghaziabad</center></h1>
+    </div>
+  )
+}
+
+export default Header
