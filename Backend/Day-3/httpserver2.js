00a4b6f434f6e750af20dd513f6d05fc663422c6
Deepanshu <paldipanshu060@gmail.com>
2025-02-14 15:34:36 +0530

    day3 modified

--- Backend/Day-3/httpserver2.js
+++ Backend/Day-3/httpserver2.js
@@ -1,15 +1,16 @@
 const http =require("http");
 const port=3002;
 const server=http.createServer(async(req,res)=>{
-    res.setHeader("Content-type","application/json");
+    res.setHeader("Content-type","text/html");
     res.statusCode=200;
     const data= await fetch("http://dummyjson.com/recipes");
     const jsonndata= await data.json();
     const newjsondata = jsonndata.recipes;
-    const names = newjsondata.map((re)=>{
-        return re.name;
+    newjsondata.map((re)=>{
+        const rdata="<h1 style='color:red' >"+re.name+"</h1>";
+        res.write(rdata)
     })
-    res.end(JSON.stringify(names));
+    res.end();
 });
 server.listen(port,(err)=>{
     try{
